954ea52c9e0f15c9fe6c1877b8023dbf8ad72208
DAC-info <gabch15@hotmail.com>
2023-04-17 21:17:26 -0500

    Integrating contract

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env:{MPH_CONTRACT_ADDRESS:process.env.MPH_CONTRACT_ADDRESS},
+  swcMinify:false
 }
 
 module.exports = nextConfig
